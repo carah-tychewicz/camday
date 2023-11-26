2363553cbb69db008b52310a36d69c050a2bcff0
Carah Tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2023-11-26 01:10:21 -0600

    initial commit. added my contribution. waiting for trevor and mike.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1>HAPPY BIRTHDAY CAM!!!</h1>
+      <div className='row align-items-start'>
+        <img className='col-4' src="https://res.cloudinary.com/drz0iulpo/image/upload/v1700976021/tayne-nude-tayne_locro2.gif" />
+        <div className='col-4'>
+          <p>I hope this birthday webpage brings you joy. Trevor, Mike, and I have all added birthday messages to you. Keep scrolling!</p>
+          <h1>From Carah:</h1>
+          <p>Hi Cam! Congratulations on making it through another year.
+            You're absolutely killing it out there. 
+            You said you wanted more chili so I decided to give you the recipe. 
+            Now you can make it anytime you want! 
+            I hope you enjoy this gift, have a nice birthday.
+          </p>
+          <h2>Ingredients:</h2>
+          <ul>
+            <li>1 - 2 large red or yellow onion</li>
+            <li>1lb sliced white mushrooms</li>
+            <li>1 bunch of celery</li>
+            <li>2 yellow and/or red bell peppers</li>
+            <li>several jalapenos (depends on heat tolerance)</li>
+            <li>1 can of corn</li>
+            <li>2 15oz cans of kidney beans</li>
+            <li>1 15oz can of black beans</li>
+            <li>1 15oz can of navy beans</li>
+            <li>1 30oz can of crushed tomatoes.</li>
+            <li>32oz tomato juice</li>
+            <li>2 - 3 packets of chili seasoning mix. I usually use McCormick or La Preferida. Season to taste.</li>
+            <li>A couple of dashes of Goya Adobo Con Pimienta seasoning. Don't go crazy with this stuff, a little bit goes a long way. Too much will make the dish very salty.</li>
+          </ul>
+          <h2>Instructions:</h2>
+          <ol>
+            <li>Rinse all raw veggies in cold water and remove all dirt and debris. Be sure to remove base and flowery head to seperate celery stalks in order to rinse nooks and crannies.</li>
+            <li>cut onion in half then slice halves into slivers</li>
+            <li>slice celery about 1/4" - 1/2". Think "tiny green medallions"</li>
+            <li>finely dice bell peppers and jalapenos</li>
+            <li>add all veggies including mushrooms to large cooking pot.</li>
+            <li>drain canned corn and beans, then add to pot.</li>
+            <li>Add crushed tomatoes and tomato juice.</li>
+            <li>mix ingredients</li>
+            <li>add 1 packet of chili mix and a couple dashes of adobo</li>
+            <li>mix again</li>
+            <li>set stove to high heat and bring pot to a boil</li>
+            <li>once boiling, stir pot, then cover with lid and reduce to low heat.</li>
+            <li>simmer for an hour at low heat, stir the pot every 15 minutes.</li>
+            <li>at 30 minutes, taste test the chili. continue to season to taste. i usually end up throwing another packet of chili seasoning in.</li>
+            <li>after one hour, stir pot, return to boil and reduce for about 5 minutes, strring the pot every 1 minute.</li>
+            <li>remove from heat and allow it to cool for a few minutes.</li>
+          </ol>
+          <p>There's a lot of prep work that goes into it, but once that's done you mostly get to sit back for an hour while it cooks itself. Good luck and enjoy! Text me if you have questions.</p>
+        </div> 
+        <img className='col-4' src="https://res.cloudinary.com/drz0iulpo/image/upload/v1700976021/tayne-nude-tayne_locro2.gif" />
+      </div>
     </div>
   );
 }
