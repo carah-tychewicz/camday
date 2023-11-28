15a03518aeec4168bed6e3c3705facebceab76d1
Carah Tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2023-11-28 00:07:23 -0600

    setting up images for iteration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-
+let i = "01";
 function App() {
   return (
     <div className="container text-center content-bg col-6">
       <h1>HAPPY BIRTHDAY CAM!!!</h1>
       <h3>We got you possum memes!</h3>
+      <img src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_" + i + ".jpg"} />
     </div>
   );
 }
