8e517b29179f326f9aa8c3ff9f970bcd9986c23c
Carah Tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2023-11-28 00:15:18 -0600

    converting number to string to loop through image urls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-let i = "01";
+let i = 0;
+i = i.toString();
+
 function App() {
   return (
     <div className="container text-center content-bg col-6">
       <h1>HAPPY BIRTHDAY CAM!!!</h1>
       <h3>We got you possum memes!</h3>
-      <img src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_" + i + ".jpg"} />
+      <img src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_0" + i + ".jpg"} />
     </div>
   );
 }
