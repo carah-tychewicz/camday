795e29c74b9c142d3282289c205d70fc3b679c91
Carah Tychewicz <10439123+carah-tychewicz@users.noreply.github.com>
2023-12-04 00:19:03 -0600

    battery about to die

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
 
-
 function App() {
+
+  let pinataBreak = false;
+
+const setDisplay = () => {
+  pinataBreak = true;
+  return pinataBreak;
+}
   return (
     <div className="container text-center content-bg col-9">
       <h1>HAPPY BIRTHDAY CAM!!!</h1>
@@ -13,89 +19,93 @@ function App() {
       </div>
       <h1>CLICK PLAY FOR BIRTHDAY SONG</h1>
       <div className='row video'>
-        <iframe width="853" height="480" 
+        <iframe width="853" height="480" is
           src="https://www.youtube.com/embed/BpgBhUE0-8U" title="Metalocalypse: Dethklok | Birthday Dethday  | Adult Swim" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
         </iframe>
       </div>
       <h1>CLICK FOR GIFTS</h1>
       <div className='row'>
         <div className='col'>
-          <img className="img-fluid pinata" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_pinata.png"} />
+          <button onClick={setDisplay()}>
+            <img className="img-fluid pinata" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_pinata.png"} />
+          </button>
         </div>
       </div>
-      <div className='row'>
-        <iframe width="892" height="428" 
-          src="https://www.youtube.com/embed/oz0Y8kS9sus" title="no fighting" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-        </iframe>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_00.png"} />
+      <div style={{display: `${pinataBreak ? '' : 'none'}`}}>
+        <div className='row'>
+          <iframe width="892" height="428" 
+            src="https://www.youtube.com/embed/oz0Y8kS9sus" title="no fighting" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+          </iframe>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_01.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_00.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_02.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_01.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_03.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_02.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_04.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_03.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_05.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_04.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_06.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_05.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_07.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_06.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_08.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_07.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_09.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_08.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_10.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_09.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_11.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_10.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_12.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_11.png"} />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_13.png"} />
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_12.png"} />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <img className="img-fluid" src={"https://res.cloudinary.com/drz0iulpo/image/upload/v1701149933/camday/camday_13.png"} />
+          </div>
         </div>
       </div>
     </div>
